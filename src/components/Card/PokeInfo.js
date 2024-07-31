927178ef8203ec0081b3296f618f23a7f52b6b16
ocean6954 <oceanan699@gmail.com>
2024-08-01 02:31:25 +0900

    [update]Infoの各要素のheight調整

--- src/components/Card/PokeInfo.js
+++ src/components/Card/PokeInfo.js
@@ -11,14 +11,60 @@ const StyledInfoContainer = styled.div`
   background-color: red;
   width: 90%;
   margin: 0 auto;
-  height: 100%;
-  /* position: absolute; */
-  /* top: 50%; */
+  height: 90%;
 `;
 
-const StyledNavbar = styled.div``;
+const StyledInfoNav = styled.div`
+  height: 60px;
+  background-color: blue;
+  margin-bottom: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
-const StyledInformation = styled.div``;
+const StyledInformation = styled.div`
+  margin: 0 auto;
+  height: 85%;
+  width: 90%;
+`;
+
+const StyledInfoHead = styled.div`
+  display: flex;
+  background-color: green;
+
+  justify-content: center;
+  align-items: center;
+  height: 10%;
+`;
+
+const StyledTable = styled.table`
+  background-color: orange;
+  width: 100%;
+  margin: 2% 0;
+  height: 46%;
+
+  & th {
+    background-color: aqua;
+    width: 45%;
+  }
+`;
+
+const StyledDescription = styled.p`
+  font-family: "DotGothic16", sans-serif;
+  font-weight: 400;
+  font-style: normal;
+  font-size: 28px;
+  width: 85%;
+  height: 37%;
+  background-color: purple;
+`;
+
+const StyledButton = styled.button`
+  position: absolute;
+  right: 10%;
+  bottom: -10;
+`;
 
 const PokeInfo = ({ featuredPokemon, onClick }) => {
   const [japaneseName, setJapaneseName] = useState("");
@@ -42,51 +88,70 @@ const PokeInfo = ({ featuredPokemon, onClick }) => {
     setDesIndex(index === 0 ? 1 : 0);
   };
 
+  const SplitByFullWidthSpace = (text) => {
+    // 全角スペースで文字列を分割
+    const parts = text.split(/(　)/);
+    return (
+      <div>
+        {parts.map((part, index) => (
+          <span key={index}>{part}</span>
+        ))}
+      </div>
+    );
+  };
   return (
     <>
       <StyledInfoContainer>
-        <StyledNavbar></StyledNavbar>
-        <StyledInformation>
+        <StyledInfoNav>
           <div>{japaneseName}</div>
-          <div>
-            <div>タイプ</div>
-            {featuredPokemon.types.map((type) => {
-              const { japanese_name } = getJapaneseType(type);
-              return (
-                <span key={featuredPokemon.name + type.type.name}>
-                  {japanese_name}
-                </span>
-              );
-            })}
-          </div>
-          <div>
-            <div>
-              <p>重さ: {(featuredPokemon.weight / 10).toFixed(1)}kg</p>
-            </div>
-            <div>
-              <p>高さ: {(featuredPokemon.height / 10).toFixed(1)}m</p>
-            </div>
-            <div>
-              <p>アビリティ: {featuredPokemon.abilities[0].ability.name}</p>
-            </div>
-            {descriptions.length > 0 && (
-              <div>
-                <p>{descriptions[desIndex].flavor_text}</p>
-                <p>
-                  {descriptions[desIndex]?.version === undefined
-                    ? "???"
-                    : descriptions[desIndex].version}
-                  より
-                </p>
-
-                <span onClick={() => toggleDescriptions(desIndex)}>
-                  <FaArrowsRotate color={"white"} />
-                </span>
-              </div>
-            )}
-          </div>
-          <button onClick={onClick}>戻る</button>
+        </StyledInfoNav>
+        <StyledInformation>
+          <StyledInfoHead>
+            <p>〇〇ポケモン</p>
+          </StyledInfoHead>
+          <StyledTable>
+            <tbody>
+              <tr>
+                <th>タイプ</th>
+                {featuredPokemon.types.map((type) => {
+                  const { japanese_name } = getJapaneseType(type);
+                  return (
+                    <td key={featuredPokemon.name + type.type.name}>
+                      {japanese_name}
+                    </td>
+                  );
+                })}
+              </tr>
+              <tr>
+                <th>高さ</th>
+                <td>{(featuredPokemon.height / 10).toFixed(1)}m</td>
+              </tr>
+              <tr>
+                <th>重さ</th>
+                <td>{(featuredPokemon.weight / 10).toFixed(1)}kg</td>
+              </tr>
+              {/* <tr>
+                <th>アビリティ: {featuredPokemon.abilities[0].ability.name}</th>
+              </tr> */}
+            </tbody>
+          </StyledTable>
+          {descriptions.length > 0 && (
+            <StyledDescription>
+              {SplitByFullWidthSpace(descriptions[desIndex].flavor_text)}
+              {/* {descriptions[desIndex].flavor_text} */}
+            </StyledDescription>
+          )}
+          <p>
+            {descriptions[desIndex]?.version === undefined
+              ? "???"
+              : descriptions[desIndex].version}
+            より
+            <span onClick={() => toggleDescriptions(desIndex)}>
+              <FaArrowsRotate color={"white"} />
+            </span>
+          </p>
         </StyledInformation>
+        <StyledButton onClick={onClick}>戻る</StyledButton>
       </StyledInfoContainer>
     </>
   );
